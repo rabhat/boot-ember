86bdc0bb405b036e700dba968af58364ba610e89
Raghs <smile2raghu@gmail.com>
2016-08-24 09:38:15 +0530

    Initial Commit

---
+++ backend/src/main/java/zruti/ZrutiEmberBootApplication.java
@@ -0,0 +1,13 @@
+package zruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrutiEmberBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZrutiEmberBootApplication.class, args);
+	}
+}
+
---
+++ backend/src/main/java/zruti/api/SampleResource.java
@@ -0,0 +1,18 @@
+package zruti.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by rbhat on 23/08/16.
+ */
+@RestController
+public class SampleResource {
+
+    @RequestMapping(value = "/api/greeting", method = RequestMethod.GET)
+    public String message() {
+        return new String("Welcome To Spring Boot Ember");
+    }
+
+}
